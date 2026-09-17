fc9d61e71639cb6e87d0b457f876e433894d9471
Ananya21467 <chaudharyananya267@gmail.com>
2026-09-17 21:26:43 +0530

    Update login API URL to HTTPS

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.querySelector("button").onclick = async () => {
     try {
 
         // Send login details to backend
-        const response = await fetch("http://hexahive-project.onrender.com/api/auth/login", {
+        const response = await fetch("https://hexahive-project.onrender.com/api/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
