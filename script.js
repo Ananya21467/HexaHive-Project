22fe9d5220f1dafbd0552f955147b98a9895c224
Ananya21467 <chaudharyananya267@gmail.com>
2026-09-17 21:23:45 +0530

    Update login API endpoint and refactor code

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ document.querySelector("button").onclick = async () => {
     try {
 
         // Send login details to backend
-        const response = await fetch("http://localhost:5000/api/auth/login", {
+        const response = await fetch("http://hexahive-project.onrender.com/api/auth/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
