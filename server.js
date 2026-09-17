cc67b8679344c131a5d027620df6273e6e25fe76
Ananya21467 <chaudharyananya267@gmail.com>
2026-09-17 19:57:15 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const authRoutes = require("./routes/authRoutes");
+const applicationRoutes = require("./routes/applicationroutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.send("HexaHive Backend is running!");
+});
+
+// Authentication routes
+app.use("/api/auth", authRoutes);
+app.use("/api/applications", applicationRoutes);
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("MongoDB connected successfully!");
+
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed:", error.message);
+    });
